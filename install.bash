36f2cdcd1ef63c10f9c576a533008eae8d4557fa
sb <srbdev@users.noreply.github.com>
2021-02-03 07:27:46 -0800

    Add updated docker-compose version

--- install.bash
+++ install.bash
@@ -128,7 +128,7 @@ install_docker () {
 
   sudo docker run --rm hello-world
 
-  sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
   docker-compose --version
 
